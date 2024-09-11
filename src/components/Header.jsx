b16d7f5c6c0f8f08868d3d9579ec178cbea515c2
Medido1 <medi1222@gmail.com>
2024-09-11 21:16:36 +0100

    Inital setup
      - Set css reset
      - Create Header component with logo and toggle button
      - Create header.module.css

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import styles from '../styles/header.module.css';
+import logo from '../assets/images/stack-of-books.png';
+import closeIcon from '../assets/images/icon-close.svg';
+import hamburgerIcon from '../assets/images/icon-hamburger.svg';
+
+function Header({isMobile}) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  function openMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+  return (
+    <header className={styles.header}>
+      <div className={styles.header_top}>
+        <img src={logo} alt="books logo" className={styles.logo} />
+        {isMobile && 
+          <>
+            <button 
+              className='toggle_menu'
+              aria-label='toggle menu button'
+              onClick={openMenu}
+            >
+              <img 
+                className='icon'
+                src={isMenuOpen ? closeIcon : hamburgerIcon}
+                alt={isMenuOpen? "close menu icon": "open menu icon"}
+              />
+            </button>
+          </>
+        }
+      </div>
+    </header>
+  )
+}
+
+export default Header;
