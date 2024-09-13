15b238496784de1187b50a5d3e20c49fd617a298
Medido1 <medi1222@gmail.com>
2024-09-13 14:12:00 +0100

    Add buttons to scroll to next and previous items

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import styles from '../styles/main.module.css';
+import rightArrow from '../assets/images/right-arrow.png';
+import leftArrow from '../assets/images/left-arrow.png';
 
 function Main() {
   const [currentData, setCurrentData] = useState([]);
@@ -36,10 +38,24 @@ function Main() {
     return stars;
   }
 
+  function showNext(){
+    if (index >= currentData.length -1){
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  }
+
+  function showPrevious() {
+    if (index === 0){
+      setIndex(currentData.length - 1)
+    } else {
+      setIndex(index - 1)
+    }
+  }
+
   function showData() {
-    console.log(currentData[0]);
-    let rating = currentData[0].rating.rate;
-    console.log(Math.ceil(rating));
+    console.log(currentData);
   }
   return (
     <main className={styles.main}>
@@ -47,6 +63,20 @@ function Main() {
       <div className={styles.itemsContainer}>
         {currentData.length > 0 ? (
           <div className={styles.item}>
+            <button 
+              onClick={showNext}
+              aria-label='next button' 
+              className={styles.arrowRight}
+            >
+              <img src={rightArrow} alt="next button" />
+            </button>
+            <button
+              onClick={showPrevious}
+              aria-label='previous button'
+              className={styles.arrowLeft}
+            >
+              <img src={leftArrow} alt="previous button" />
+            </button>
             <h2>{currentData[index].title}</h2>
             <img 
               src={currentData[index].image} 
